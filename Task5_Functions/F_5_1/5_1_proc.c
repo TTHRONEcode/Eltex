fb3f8ceeca95c5f3963d1991fec0f92b2abfe4f3
TTHRONEcode <ttuda6upka.official@gmail.com>
2024-07-03 11:18:33 +0700

    Change 5_1 to KISS model

--- Task5_Functions/F_5_1/5_1_proc.c
+++ Task5_Functions/F_5_1/5_1_proc.c
@@ -1,11 +1,11 @@
-// 1. Переписать абонентский справочник с использованием функций.
-
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #define STRUCT_SIZE 100
 #define STRUCT_ELEMENTS_ARRAY_SIZE 10
+
+enum { ADD = 1, DELETE, SEARCH, PRINT_ALL, EXIT };
 
 struct AbonentList {
   char name[STRUCT_ELEMENTS_ARRAY_SIZE + 1];
@@ -15,52 +15,56 @@ struct AbonentList {
 
 struct AbonentList directory[STRUCT_SIZE];
 char g_buffer_name[STRUCT_ELEMENTS_ARRAY_SIZE + 1];
-int g_free_directory, g_menu_num, g_i, g_j;
+int g_free_directory, g_i, g_j;
 bool g_was_changed, g_was_deleted;
 
-void UtilityClearScanf() {
+void Proc_ClearScanf() {
   int c;
   while ((c = fgetc(stdin)) != EOF && c != '\n')
     ;
 }
 
-void UtilityClearBuffer() {
+void Proc_ClearBuffer() {
   for (g_i = 0; g_i < STRUCT_ELEMENTS_ARRAY_SIZE + 1; g_i++) {
     g_buffer_name[g_i] = 0;
   }
 }
 
-// 1) Добавление абонента
-void DirectoryAdd() {
-  printf("*1) Добавление абонента\n");
+void Proc_FindFreeSpace() {
+  g_was_changed = false;
+  for (g_j = 0; g_j < STRUCT_SIZE; g_j++) {
+    if (directory[g_j].name[0] == 0) {
+      g_free_directory = g_j;
+      g_was_changed = true;
+      break;
+    }
+  }
+
+  if (!g_was_changed)
+    g_free_directory = -1;
+}
+
+// Добавление абонента
+void Proc_DirAdd() {
+  printf("*%d) Добавление абонента\n", ADD);
 
   if (g_free_directory != -1) {
     printf("*Введите имя абонента (%i символов): \n",
            STRUCT_ELEMENTS_ARRAY_SIZE);
     scanf("%10s", directory[g_free_directory].name);
-    UtilityClearScanf();
+    Proc_ClearScanf();
 
     printf("*Введите фамилию абонента (%i символов): \n",
            STRUCT_ELEMENTS_ARRAY_SIZE);
     scanf("%10s", directory[g_free_directory].second_name);
-    UtilityClearScanf();
+    Proc_ClearScanf();
 
     printf("*Введите телефон абонента (%i символов): \n",
            STRUCT_ELEMENTS_ARRAY_SIZE);
     scanf("%10s", directory[g_free_directory].tel);
-    UtilityClearScanf();
+    Proc_ClearScanf();
 
-    g_was_changed = false;
-    for (g_j = 0; g_j < STRUCT_SIZE; g_j++) {
-      if (directory[g_j].name[0] == 0) {
-        g_free_directory = g_j;
-        g_was_changed = true;
-        break;
-      }
-    }
-
-    if (!g_was_changed)
-      g_free_directory = -1;
+    Proc_FindFreeSpace();
 
     printf("*Абонент успешно добавлен!\n");
   } else {
@@ -70,15 +74,14 @@ void DirectoryAdd() {
   }
 }
 
-// 2) Удаление абонента
-void DirectoryDelete() {
-  printf("*2) Удаление абонента\n");
+// Удаление абонента
+void Proc_DirDelete() {
+  printf("*%d) Удаление абонента\n", DELETE);
   printf("*Введите имя абонентов для удаления:\n");
 
-  UtilityClearBuffer();
+  Proc_ClearBuffer();
 
   scanf("%10s", g_buffer_name);
-  UtilityClearScanf();
 
   g_was_deleted = false;
   for (g_i = 0; g_i < STRUCT_SIZE; g_i++) {
@@ -115,15 +118,15 @@ void DirectoryDelete() {
     printf("*Абонентов с именем %s не найдено.\n", g_buffer_name);
 }
 
-// 3) Поиск абонентов по имени
-void DirectoryFind() {
-  printf("*3) Поиск абонентов по имени\n");
+//  Поиск абонентов по имени
+void Proc_DirSearch() {
+  printf("*%c) Поиск абонентов по имени\n", SEARCH);
   printf("*Введите имя абонентов для поиска: ");
 
-  UtilityClearBuffer();
+  Proc_ClearBuffer();
 
   scanf("%10s", g_buffer_name);
-  UtilityClearScanf();
+  Proc_ClearScanf();
 
   printf("*Найденые абоненты с именем %s:\n", g_buffer_name);
   for (g_i = 0; g_i < STRUCT_SIZE; g_i++) {
@@ -141,9 +144,10 @@ void DirectoryFind() {
   }
 }
 
-// 4) Вывод всех записей
-void DirectoryPrint() {
-  printf("*4) Вывод всех записей:\n");
+//  Вывод всех записей
+void Proc_DirPrintAll() {
+  printf("*%c) Вывод всех записей:\n", PRINT_ALL);
+
   g_was_changed = false;
 
   for (g_i = 0; g_i < STRUCT_SIZE; g_i++) {
@@ -157,60 +161,8 @@ void DirectoryPrint() {
     printf("*Список пуст. Самое время добавить абонента!\n");
 }
 
-// 5) Выход
-void DirectoryExit() {
-  printf("*5) Выход\n*Выходим...\n");
+// Выход
+void Proc_DirExit() {
+  printf("*%c) Выход\n*Выходим...\n", EXIT);
   exit(0);
-}
-
-// Меню
-void DirectoryMenu() {
-  while (g_menu_num != 5) {
-    g_menu_num = -1;
-    printf("\n*Абонентский справочник*\n*Меню:\n"
-           "1) Добавить абонента\n"
-           "2) Удалить абонента\n"
-           "3) Поиск абонентов по имени\n"
-           "4) Вывод всех записей\n"
-           "5) Выход\n");
-    printf("*Выберите пункт меню: ");
-    scanf("%1d", &g_menu_num);
-    UtilityClearScanf();
-
-    while (g_menu_num < 0 || g_menu_num > 5) {
-      printf("\n*Нужно ввести число от 1 до 5!\n");
-      printf("*Выберите пункт меню: ");
-      scanf("%1d", &g_menu_num);
-      UtilityClearScanf();
-    }
-    printf("\n");
-
-    switch (g_menu_num) {
-    case 1:
-      DirectoryAdd();
-      break;
-
-    case 2:
-      DirectoryDelete();
-      break;
-
-    case 3:
-      DirectoryFind();
-      break;
-
-    case 4:
-      DirectoryPrint();
-      break;
-
-    default:
-      DirectoryExit();
-      break;
-    }
-  }
-}
-
-int main() {
-  DirectoryMenu();
-
-  return 0;
 }
---
+++ Task5_Functions/F_5_1/5_1_std.c
@@ -0,0 +1,50 @@
+#include "5_1_proc.c"
+
+int g_menu_num;
+
+// Меню
+void Std_DirectoryMenu() {
+  while (g_menu_num != EXIT) {
+    g_menu_num = -1;
+    printf("\n*Абонентский справочник*\n*Меню:\n"
+           "%d) Добавить абонента\n"
+           "%d) Удалить абонента\n"
+           "%d) Поиск абонентов по имени\n"
+           "%d) Вывод всех записей\n"
+           "%d) Выход\n",
+           ADD, DELETE, SEARCH, PRINT_ALL, EXIT);
+    printf("*Выберите пункт меню: ");
+    scanf("%1d", &g_menu_num);
+    Proc_ClearScanf();
+
+    while (g_menu_num < ADD || g_menu_num > EXIT) {
+      printf("\n*Нужно ввести число от 1 до 5!\n");
+      printf("*Выберите пункт меню: ");
+      scanf("%1d", &g_menu_num);
+      Proc_ClearScanf();
+    }
+    printf("\n");
+
+    switch (g_menu_num) {
+    case ADD:
+      Proc_DirAdd();
+      break;
+
+    case DELETE:
+      Proc_DirDelete();
+      break;
+
+    case SEARCH:
+      Proc_DirSearch();
+      break;
+
+    case PRINT_ALL:
+      Proc_DirPrintAll();
+      break;
+
+    default:
+      Proc_DirExit();
+      break;
+    }
+  }
+}
