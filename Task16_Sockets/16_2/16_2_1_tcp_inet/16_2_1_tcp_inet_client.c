c65bddbf2ef2d4b7981907c21c8f6b5ea7663320
TTHRONEcode <ttuda6upka.official@gmail.com>
2024-08-13 12:44:44 +0700

    feat: do the Task16_2 _1_tcp_inet

---
+++ Task16_Sockets/16_2/16_2_1_tcp_inet/16_2_1_tcp_inet_client.c
@@ -0,0 +1,88 @@
+#include <err.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include "./16_2_1_tcp_inet.h"
+
+#define GET_ENUM_CHAR(enum_name) (enum_name + '0')
+
+static void
+CheckError (int err_int, char *err_str, int caller_line)
+{
+  if (err_int < 0)
+    {
+      err (EXIT_FAILURE, "%s: %d", err_str, caller_line);
+    }
+}
+
+static void
+TalkToServer (int sock_fd)
+{
+  char msg_recv[255] = { 0 };
+  char typed_char[STR_SIZE_MAX] = { 0 };
+
+  do
+    {
+      printf ("\n*Введите номер действия:*\n"
+              "*%c. Запросить время/дату сервера*\n"
+              "*%c. Выйти*\n"
+              "> ",
+              k_MSG_T_GIVE_ME_TIME, k_MSG_T_EXIT);
+
+      typed_char[0] = getchar ();
+
+      int c;
+      while ((c = fgetc (stdin)) != EOF && c != '\n')
+        ;
+
+      if (typed_char[0] == k_MSG_T_GIVE_ME_TIME)
+        {
+          CheckError (send (sock_fd, (char *)typed_char, STR_SIZE_MAX, 0),
+                      "send", __LINE__);
+
+          CheckError (recv (sock_fd, (char *)msg_recv, STR_SIZE_MAX, 0),
+                      "recv", __LINE__);
+
+          CheckError (printf ("%s\n", msg_recv), "printf", __LINE__);
+        }
+      else
+        {
+          CheckError (send (sock_fd, (char *)typed_char, STR_SIZE_MAX, 0),
+                      "send", __LINE__);
+
+          break;
+        }
+
+      typed_char[0] = 0;
+    }
+  while (1);
+}
+
+int
+main ()
+{
+  struct sockaddr_in serv;
+  int sock_fd;
+
+  char *msg_send = "Hi!!";
+  char msg_recv[STR_SIZE_MAX];
+
+  socklen_t len = sizeof (struct sockaddr);
+
+  serv.sin_family = AF_INET;
+  serv.sin_port = htons (12345);
+  serv.sin_addr.s_addr = INADDR_ANY;
+
+  CheckError (sock_fd = socket (AF_INET, SOCK_STREAM, 0), "socket", __LINE__);
+  CheckError (connect (sock_fd, (struct sockaddr *)&serv, len), "connect",
+              __LINE__);
+
+  TalkToServer (sock_fd);
+
+  CheckError (close (sock_fd), "close", __LINE__);
+
+  return 0;
+}
---
+++ Task16_Sockets/16_2/16_2_1_tcp_inet/16_2_1_tcp_inet_server.c
@@ -0,0 +1,141 @@
+#include <err.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <time.h>
+#include <unistd.h>
+
+#include "./16_2_1_tcp_inet.h"
+
+struct sockaddr_in serv, client;
+int sock_fd;
+
+socklen_t len = sizeof (struct sockaddr);
+
+static void
+PrintError (char *err_str, int caller_line)
+{
+  err (EXIT_FAILURE, "%s: %d", err_str, caller_line);
+}
+
+static void
+CheckError (int err_int, char *err_str, int caller_line)
+{
+  if (err_int < 0)
+    PrintError (err_str, caller_line);
+}
+
+void
+ProcSocketClient (int loc_sock_fd, int loc_clients_fd, int this_proc_num)
+{
+  char msg_recv[STR_SIZE_MAX] = { 0 };
+
+  CheckError (printf ("\n*Process №%d is getting started with fd %d*\n",
+                      this_proc_num, loc_clients_fd),
+              "printf", __LINE__);
+  do
+    {
+      CheckError (recv (loc_clients_fd, (char *)msg_recv, STR_SIZE_MAX, 0),
+                  "recv", __LINE__);
+
+      if (msg_recv[0] == k_MSG_T_GIVE_ME_TIME)
+        {
+          time_t time_not_str = time (NULL);
+          char *time_to_str = asctime (gmtime (&time_not_str));
+
+          CheckError (
+              send (loc_clients_fd, (char *)time_to_str, STR_SIZE_MAX, 0),
+              "send", __LINE__);
+        }
+      else
+        {
+          break;
+        }
+
+      CheckError (
+          printf ("*%d*\n*The request has been successfully processed*\n",
+                  this_proc_num),
+          "printf", __LINE__);
+    }
+  while (1);
+
+  CheckError (printf ("*Process №%d is exiting*\n", this_proc_num), "printf",
+              __LINE__);
+
+  CheckError (close (loc_clients_fd), "close", __LINE__);
+  CheckError (close (loc_sock_fd), "close", __LINE__);
+
+  exit (EXIT_SUCCESS);
+}
+
+static void
+SocketHandler ()
+{
+  pid_t fork_val = 0;
+  char message_buf[STR_SIZE_MAX] = { 0 };
+  int proc_count = 0, clients_fd = 0;
+
+  do
+    {
+      CheckError (printf ("[ MAIN PROC: Wait for client ]\n"), "printf",
+                  __LINE__);
+      CheckError (clients_fd
+                  = accept (sock_fd, (struct sockaddr *)&client, &len),
+                  "accept", __LINE__);
+      CheckError (printf ("[ MAIN PROC: Client connected with fd = %d ]\n\n",
+                          clients_fd),
+                  "printf", __LINE__);
+
+      proc_count++;
+
+      if ((fork_val = fork ()) == 0)
+        ProcSocketClient (sock_fd, clients_fd, proc_count);
+      else if (fork_val == -1)
+        PrintError ("fork", __LINE__);
+    }
+  while (1);
+}
+
+static void
+SocketInitProcClose ()
+{
+  CheckError (printf ("-> HELP: Want to exit? Press Ctrl-C! <-\n\n"), "printf",
+              __LINE__);
+
+  serv.sin_family = AF_INET;
+  serv.sin_port = htons (12345);
+  serv.sin_addr.s_addr = INADDR_ANY;
+
+  CheckError (sock_fd = socket (AF_INET, SOCK_STREAM, 0), "socket", __LINE__);
+
+  CheckError (bind (sock_fd, (struct sockaddr *)&serv, len), "bind", __LINE__);
+
+  CheckError (listen (sock_fd, 2), "listen", __LINE__);
+
+  SocketHandler ();
+
+  CheckError (close (sock_fd), "close", __LINE__);
+}
+
+static void
+SigExit ()
+{
+  CheckError (close (sock_fd), "close", __LINE__);
+
+  CheckError (printf ("\n"), "printf", __LINE__);
+
+  exit (EXIT_SUCCESS);
+}
+
+int
+main ()
+{
+  signal (SIGINT, SigExit);
+
+  SocketInitProcClose ();
+
+  return 0;
+}
